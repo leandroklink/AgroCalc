006680d275398dbbd3dc02a3185fe31839326660
Leandro <leandroklink13@gmail.com>
2026-06-01 10:35:41 -0400

    adicionando comentários no javascript"

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,4 @@
+//capturando botao do html
 const botaoTema = document.getElementById('tema-btn');
 
 
@@ -5,16 +6,20 @@ if (localStorage.getItem('tema') === 'escuro') {
     document.body.classList.add('dark-mode');
 
 }
-
+//adiconando evento para o clique do botao
 botaoTema.addEventListener('click', () => {
     
     document.body.classList.toggle('dark-mode');
-
+    //verificando if do botao para saber se o mesmo está ou não com modo escuro ativado
     if (document.body.classList.contains('dark-mode')) {
+        //setando botao para o modo escuro 
         localStorage.setItem('tema', 'escuro');
+        //alterando conteudo do botao para um sol
         botaoTema.textContent = "🌞"
     } else {
+        //setando tema para claro
         localStorage.setItem('tema', 'claro');
+        //alterando conteudo do botao para uma lua
         botaoTema.textContent = "🌙"
     }
 
